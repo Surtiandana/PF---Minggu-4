8ada528291021af344b202200e0d0b5e535cbb52
surtiandana <dhofirohazzahlabibah@gmail.com>
2024-03-24 05:24:37 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,93 @@ use App\Http\Controllers\RouteController;
 use Illuminate\Support\Facades\Route;
 
 
+// Route::get('/routing', function() {
+//     return view('routing');
+// });
+// Route::get('/basic_routing', function() {
+//     return 'Hello World';
+// });
+// Route::view('/view_route', 'view_route');
+// Route::view('/view_route', 'view_route', ['name' => 'Purnama']);
+// Route::get('/controller_route', [RouteController::class, 'index']);
+// Route::redirect('/', '/routing');
+// Route::get('/user/{id}', function($id) {
+
+//     return "User Id: ".$id;
+// });
+// Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
+//     return "Post Id: ".$postId.", Comment Id: ".$commentId;
+// });
+
+// Route::get('username/{name?}', function($name = null) {
+//     return 'Username: '.$name;
+// });
+
+// Route::get('/title/{title}', function($title) {
+//     return "Title: ".$title;
+// })->where('title', '[A-Za-z]+');
+
+// Route::get('/profile/{profileId}', [RouteController::class, 'profile'])->name('profileRouteName');
+
+// // Route::get('/route_priority/{rpId}', function($rpId) {
+// //     return "This is Route One";
+// // });
+// Route::get('/route_priority/user', function() {
+//     return "This is Route 1";
+// });
+// Route::get('/route_priority/user', function() {
+//     return "This is Route 2";
+// });
+
+// Route::fallback(function() {
+//     return 'This is Fallback Route';
+// });
+
+// Route::prefix('admin')->name('admin.')->group(function() {
+//     Route::get('/dashboard', function() {
+//         return "This is admin dashboard";
+//     })->name('dashboard');
+//     Route::get('/users', function() {
+//         return "This is user data on admin page";
+//     })->name('users');
+//     Route::get('/items', function() {
+//         return "This is item data on admin page";
+//     })->name('items');
+// });
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/routing', function() {
     return view('routing');
 });
+
 Route::get('/basic_routing', function() {
     return 'Hello World';
 });
+
 Route::view('/view_route', 'view_route');
 Route::view('/view_route', 'view_route', ['name' => 'Purnama']);
+
 Route::get('/controller_route', [RouteController::class, 'index']);
 Route::redirect('/', '/routing');
-Route::get('/user/{id}', function($id) {
 
+
+
+Route::get('/user/{id}', function($id) {
     return "User Id: ".$id;
 });
 Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
@@ -26,11 +101,8 @@ Route::get('username/{name?}', function($name = null) {
     return 'Username: '.$name;
 });
 
-Route::get('/title/{title}', function($title) {
-    return "Title: ".$title;
-})->where('title', '[A-Za-z]+');
-
 Route::get('/profile/{profileId}', [RouteController::class, 'profile'])->name('profileRouteName');
+
 
 // Route::get('/route_priority/{rpId}', function($rpId) {
 //     return "This is Route One";
@@ -56,4 +128,8 @@ Route::prefix('admin')->name('admin.')->group(function() {
     Route::get('/items', function() {
         return "This is item data on admin page";
     })->name('items');
+});
+
+Route::get('/bs_clone', function() {
+    return view('bs_clone');
 });
